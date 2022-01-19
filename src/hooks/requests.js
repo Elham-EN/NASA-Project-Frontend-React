9600bfb8e783f365521df0a8b30601cf50c3867d
Elham-EN <ilhamrezaie@gmail.com>
2022-01-19 20:07:53 +1100

    Load data from the backend API

---
+++ src/hooks/requests.js
@@ -0,0 +1,22 @@
+async function httpGetPlanets() {
+  // Load planets from our backend application and return as JSON.
+  const res = await fetch("http://localhost:8000/planets");
+  return await res.json();
+}
+
+async function httpGetLaunches() {
+  // TODO: Once API is ready.
+  // Load launches, sort by flight number, and return as JSON.
+}
+
+async function httpSubmitLaunch(launch) {
+  // TODO: Once API is ready.
+  // Submit given launch data to launch system.
+}
+
+async function httpAbortLaunch(id) {
+  // TODO: Once API is ready.
+  // Delete launch with given ID.
+}
+
+export { httpGetPlanets, httpGetLaunches, httpSubmitLaunch, httpAbortLaunch };
